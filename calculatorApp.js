9e80f701910fc5fc9b57a48d004c1995aa70e41e
Abhishek B S <abhishekmurthy1991@gmail.com>
2015-10-15 12:16:23 +0530

    Initial Calculator

---
+++ calculatorApp.js
@@ -0,0 +1,19 @@
+var calculator = {
+    add : function (x, y) {
+        return x + y;
+    },
+    sub : function (x, y) {
+        return x - y;
+    },
+    mul : function (x, y) {
+        return x * y;
+    },
+    div : function (x, y) {
+        return x / y;
+    },
+};
+
+console.log(calculator.add(2, 4));
+console.log(calculator.sub(2, 4));
+console.log(calculator.mul(2, 4));
+console.log(calculator.div(2,4));
