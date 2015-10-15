c1dff24cf9934dce96cefb9bc9bd24002aa5c177
Abhishek B S <abhbs@cisco.com>
2015-10-15 15:49:39 +0530

    Static WebServer

---
+++ StaticWebServer/server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer(function(req, res){
+    var resource = path.join(__dirname, req.url);
+    if (fs.existsSync(resource)) {
+        fs.readFile(resource, function(err, fileContents) {
+            if (err) {
+                res.statusCode = 500;
+                res.end();
+                return;
+            }
+            res.write(fileContents);
+            res.end();
+        })
+    }
+    else {
+        res.statusCode = 404;
+        res.end();
+    }
+});
+
+server.listen(8080);
+console.log('web server running on port 8080');
